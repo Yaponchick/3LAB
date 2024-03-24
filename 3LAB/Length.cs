343a93afa42b3fdbfc59c5fdcdee63a79aa1b32d
Yaponchick <sergeyzhivotov01@mail.ru>
2024-03-25 01:20:56 +0800

    Добавьте файлы проекта.

---
+++ 3LAB/Length.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3LAB
+{
+    public class Length
+    {
+        private double valueN;
+        private double valueZ;
+
+
+        public Length(double valueN, double valueZ)
+        {
+            this.valueN = valueN;
+            this.valueZ = valueZ;
+        }
+
+
+        public string Verbose()
+        {
+            return String.Format("{0}/{1}", this.valueN, this.valueZ);
+        }
+
+
+
+        public static Length operator +(Length Numerator, Length NumeratorTwo)
+        {
+            if (Numerator.valueZ == NumeratorTwo.valueZ)
+            {
+                double newNumerator = Numerator.valueN + NumeratorTwo.valueN;
+                double newDenominator = Numerator.valueZ;
+
+                Length result = new Length(newNumerator, newDenominator);
+
+                return result;
+            }
+            else
+            {
+                double newNumerator1 = Numerator.valueN * NumeratorTwo.valueZ;
+                double newNumerator2 = NumeratorTwo.valueN * Numerator.valueZ;
+                double newDenominator = Numerator.valueZ * NumeratorTwo.valueZ;
+
+                double newNumerator = newNumerator1 + newNumerator2;
+
+                Length result = new Length(newNumerator, newDenominator);
+
+                return result;
+            }
+        }
+
+        public static Length operator -(Length Numerator, Length NumeratorTwo)
+        {
+            if (Numerator.valueZ == NumeratorTwo.valueZ)
+            {
+                double newNumerator = Numerator.valueN - NumeratorTwo.valueN;
+                double newDenominator = Numerator.valueZ;
+
+                Length result = new Length(newNumerator, newDenominator);
+
+                return result;
+            }
+            else
+            {
+                double newNumerator1 = Numerator.valueN * NumeratorTwo.valueZ;
+                double newNumerator2 = NumeratorTwo.valueN * Numerator.valueZ;
+                double newDenominator = Numerator.valueZ * NumeratorTwo.valueZ;
+
+                double newNumerator = newNumerator1 - newNumerator2;
+
+                Length result = new Length(newNumerator, newDenominator);
+
+                return result;
+            }
+        }
+        public static Length operator *(Length Numerator, Length NumeratorTwo)
+        {
+                double newNumerator = Numerator.valueN * NumeratorTwo.valueN;
+                double newDenominator = Numerator.valueZ * NumeratorTwo.valueZ;
+
+                Length result = new Length(newNumerator, newDenominator);
+
+                return result;
+        }
+        public static Length operator /(Length Numerator, Length NumeratorTwo)
+        {
+            double newNumerator = Numerator.valueN * NumeratorTwo.valueZ;
+            double newDenominator = Numerator.valueZ * NumeratorTwo.valueN;
+
+            Length result = new Length(newNumerator, newDenominator);
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //Сокращение
+        public void Remains()
+        {
+            // Находим наибольший общий делитель числителя и знаменателя
+            double result = Divider(valueN, valueZ);
+
+            // Делим числитель и знаменатель на НОД
+            valueN /= result;
+            valueZ /= result;
+        }
+
+        private double Divider(double a, double b)
+        {
+            while (b != 0)
+            {
+                double temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 3LABTests/LengthTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3LAB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3LAB.Tests
+{
+    [TestClass()]
+    public class LengthTests
+    {
+        //1 Тест
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var length = new Length(10, 5);
+
+            Assert.AreEqual("10/5", length.Verbose());
+        }
+        //2 Тест
+        [TestMethod()]
+        
+        public void AddNumberTest()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 5);
+
+            Length sum = length1 + length2;
+            Assert.AreEqual("19/20", sum.Verbose());
+        }
+        //3 Тест
+        [TestMethod()]
+
+        public void AddNumberTest2()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 4);
+
+            Length sum = length1 + length2;
+            Assert.AreEqual("4/4", sum.Verbose());
+        }
+        //4 Тест
+        [TestMethod()]
+        public void Subtraction()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 5);
+
+            Length subtraction = length1 - length2;
+            Assert.AreEqual("11/20", subtraction.Verbose());
+        }
+        //5 Тест
+        [TestMethod()]
+
+        public void Subtraction2()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 4);
+
+            Length subtraction = length1 - length2;
+            Assert.AreEqual("2/4", subtraction.Verbose());
+        }
+        //6 Тест
+        [TestMethod()]
+
+        public void Multiplication()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 4);
+
+            Length multiplication = length1 * length2;
+            Assert.AreEqual("3/16", multiplication.Verbose());
+        }
+        //7 Тест
+        [TestMethod()]
+        public void Division()
+        {
+            Length length1 = new Length(3, 4);
+            Length length2 = new Length(1, 5);
+
+            Length division = length1 / length2;
+            Assert.AreEqual("15/4", division.Verbose());
+        }
+        //8 Тест
+        [TestMethod()]
+        public void Remains()
+        {
+            Length remains = new Length(1780, 500);
+            remains.Remains();
+
+            Assert.AreEqual("89/25", remains.Verbose());
+        }
+        //9 Тест
+        [TestMethod()]
+        public void Less()
+        {
+            Length length1 = new Length(5, 5);
+            Length length2 = new Length(1, 5);
+
+            Length division = length1 < length2;
+            Assert.AreEqual("15/5", division.Verbose());
+        }
+        //10 Тест
+        [TestMethod()]
+        public void More()
+        {
+            Length remains = new Length(1780, 500);
+            remains.Remains();
+
+            Assert.AreEqual("89/25", remains.Verbose());
+        }
+    }
+}
